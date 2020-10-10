f4776423b8e5d1a3759a1fc84f1efa8aca72f5f3
yemou <meeyou@tuta.io>
2020-10-10 23:45:26 +0000

    B-)

---
+++ root/media/glibc64/etc/profile.d/ksh-user.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Place this file in /etc/profile.d
+[ "$(id -u)" = "1000" ] && [ "${0##*/}" = "-su" ] || return 0
+
+# Add local bin dirs to path
+PATH="$HOME/bin:$HOME/opt/bin:$PATH"
+
+# Set XDG variables
+XDG_CACHE_HOME="$HOME/tmp/cache"
+XDG_CONFIG_HOME="$HOME/cfg"
+XDG_DATA_HOME="$HOME/data"
+
+# KSH specific variables
+ENV="$XDG_CONFIG_HOME/loksh/rc"
+HISTCONTROL="ignoredups:ignorespace"
+HISTFILE="$XDG_CACHE_HOME/loksh_history"
+HISTSIZE=1000000
+
+# Prevent $HOME pollution
+GNUPGHOME="$XDG_DATA_HOME/gnupg"
+GRIPHOME="$XDG_CONFIG_HOME/grip"
+LESSHISTFILE="$XDG_CACHE_HOME/less_history"
+PASSWORD_STORE_DIR="$HOME/doc/.pws"
+
+# Set default commands
+EDITOR="kak"
+VISUAL="kak"
+PAGER="less"
+
+# Export all the variables
+export PATH XDG_CACHE_HOME XDG_CONFIG_HOME XDG_DATA_HOME ENV HISTCONTROL \
+       HISTFILE HISTSIZE LESSHISTFILE GNUPGHOME PASSWORD_STORE_DIR VISUAL \
+       EDITOR PAGER GRIPHOME PS1
